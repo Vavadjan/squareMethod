7252b6ce9a3221aaa4f8ca4cfa6fef40960a7b1d
Vavadjan <vavadjan@gmail.com>
2023-09-27 22:57:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace squareMethod
+{
+    internal class Program
+    {
+        
+            static int SquareMethod(int side)
+        {
+            int result = side * side;
+            return result;
+        }
+        
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Программа посчитает площадь квадрата");
+            Console.WriteLine("Введите длинну стороны квадрата в метрах");
+
+            int side = Convert.ToInt32(Console.ReadLine());
+
+            int result = SquareMethod(side);
+
+            Console.WriteLine("Площадь квадрата - " + result + " метров квадратных.");
+        }
+    }
+}
